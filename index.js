6e62a31abea9b9f62ab0a18d9dc734772893c278
SHWETA SINGH <85755259+ss-shweta@users.noreply.github.com>
2021-11-05 11:51:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+
+function func() {
+    var link = document.getElementById('sp');
+    link.innerHTML += <a href="Login.html"></a>
+}
+var but = document.getElementById('but').addEventListener('click', func);
